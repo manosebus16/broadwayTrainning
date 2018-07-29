31a9129fd57d0b66cdc365305ba4b2c8904cfd8d
Manish Basnet <mbasnet911@gmail.com>
2018-07-29 21:23:58 +0545

    Variables

---
+++ datatypes.php
@@ -0,0 +1,11 @@
+<<?php 
+/*
+	Number
+	Strings
+	Arrays
+	Objects
+	Null
+	Boolean
+*/
+
+ ?>
--- variables.php
+++ variables.php
@@ -7,4 +7,30 @@
 
 	echo $sum;
 	
+	$sum = $sum + $num_1;
+	echo "<br>";
+
+	echo $sum;
+
+	echo "<br>";
+	echo "<br>";
+
+
+	const PIE_VALUE = 3.14;
+
+	define('PIE_VALUE_1', 3.1416);
+
+		echo PIE_VALUE;
+		echo "<br>";
+		echo "<br>";
+
+		echo PIE_VALUE_1;
+
+		$sum_of_PIE = PIE_VALUE+PIE_VALUE_1;
+
+		echo "<br>";
+		echo "<br>";
+
+		echo $sum_of_PIE;
+
  ?>
